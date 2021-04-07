f37fe8d60c0116af96149c05d9dc61bf50d5312d
parksubo <subo0521@hanmail.net>
2021-04-07 16:15:14 +0900

    조회기능 수정

--- src/main/java/org/cnu/realcoding/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -2,7 +2,6 @@ package org.cnu.realcoding.controller;
 
 import org.cnu.realcoding.domain.Dog;
 import org.cnu.realcoding.service.DogManagementService;
-import org.cnu.realcoding.service.DogSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +14,6 @@ public class DogController {
 
     @Autowired
     private DogManagementService dogManagementService;
-    @Autowired
-    private DogSearchService dogSearchService;
 
     @PostMapping("/dogs")
     @ResponseStatus(HttpStatus.CREATED)
@@ -35,23 +32,23 @@ public class DogController {
     // 등록된 강아지 조회
     @GetMapping("/dogs/by-name/{name}")
     public List<Dog> getDogByName(@PathVariable String name) {
-        return dogSearchService.getDogByName(name);
+        return dogManagementService.getDogByName(name);
     }
 
     @GetMapping("/dogs/by-ownerName/{ownerName}")
     public List<Dog> getDogByOwnerName(@PathVariable String ownerName) {
-        return dogSearchService.getDogByOwnerName(ownerName);
+        return dogManagementService.getDogByOwnerName(ownerName);
     }
 
     @GetMapping("/dogs/by-ownerPhoneNumber/{ownerPhoneNumber}")
     public List<Dog> getDogByOwnerPhoneNumber(@PathVariable String ownerPhoneNumber) {
-        return dogSearchService.getDogByOwnerPhoneNumber(ownerPhoneNumber);
+        return dogManagementService.getDogByOwnerPhoneNumber(ownerPhoneNumber);
     }
 
     // http://localhost:8080/dogs?name=ian&ownername=ian&owe
     @GetMapping("/dogs")
     public List<Dog> getDogByNameOwnerNameOwnerPhoneNumber(@RequestParam String name, @RequestParam String ownerName , @RequestParam String ownerPhoneNumber) {
-        return dogSearchService.getDogByNameOwnerNameOwnerPhoneNumber(name, ownerName, ownerPhoneNumber);
+        return dogManagementService.getDogByNameOwnerNameOwnerPhoneNumber(name, ownerName, ownerPhoneNumber);
     }
 
 }
---
+++ src/main/java/org/cnu/realcoding/repository/DogRepository.java
@@ -0,0 +1,97 @@
+package org.cnu.realcoding.repository;
+
+import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.exception.DogConflictException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DogRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Dog findDog(String name) {
+        return mongoTemplate
+                .findOne(
+                        Query.query(Criteria.where("name").is(name)),
+                        Dog.class
+                );
+    }
+
+    public List<Dog> findDogByName(String name) {
+        Criteria criteria = new Criteria(name);
+        Query query = new Query(criteria);
+        return mongoTemplate.find(query, Dog.class, "name");
+    }
+
+    public List<Dog> findDogByOwnerName(String ownerName) {
+        Criteria criteria = new Criteria(ownerName);
+        Query query = new Query(criteria);
+        return mongoTemplate.find(query, Dog.class, "ownerName");
+    }
+
+    public List<Dog> findDogByOwnerPhoneNumber(String ownerPhoneNumber) {
+        Criteria criteria = new Criteria(ownerPhoneNumber);
+        Query query = new Query(criteria);
+        return mongoTemplate.find(query, Dog.class, "ownerPhoneNumber");
+    }
+    public List<Dog> findDogByNameOwnerNameOwnerPhoneNumber(String name, String ownerName, String ownerPhoneNumber) {
+        Criteria criteria = new Criteria(name);
+        criteria.is(ownerName);
+        criteria.is(ownerPhoneNumber);
+        Query query = new Query(criteria);
+        return mongoTemplate.find(query, Dog.class, "ownerPhoneNumber");
+    }
+
+
+
+    public List<Dog> findAllDogs() {
+        return mongoTemplate.findAll(Dog.class);
+    }
+
+
+    public void insertDog(Dog dog) {
+        if (mongoTemplate.exists(Query.query(Criteria.where("name").is(dog.getName())), Dog.class)) {
+            Dog newDog = mongoTemplate.findOne(Query.query(Criteria.where("name").is(dog.getName())), Dog.class);
+            if (newDog.getOwnerName().equals(dog.getOwnerName()))
+                if(newDog.getOwnerPhoneNumber().equals(dog.getOwnerPhoneNumber()))
+                    throw new DogConflictException();
+        }
+        mongoTemplate.insert(dog);
+    }
+
+    public void updateDogs(String name, Dog dog) {
+        Query query = new Query(Criteria.where("name").is(name));
+        Update update = new Update();
+        update.set("name", dog.getName());
+        update.set("kind", dog.getKind());
+        update.set("ownerName", dog.getOwnerName());
+        update.set("ownerPhoneNumber", dog.getOwnerPhoneNumber());
+        update.set("medicalRecords", findDog(name).getMedicalRecords());
+        mongoTemplate.updateFirst(query, update, Dog.class);
+
+    }
+
+
+
+
+    public void modifyDogsKind(String name, String kind) {
+        mongoTemplate.updateFirst(Query.query(Criteria.where("name").is(name)),
+                Update.update("kind", kind),Dog.class);
+    }
+
+    public void addMedicalRecords(String name, String records) {
+        List<String> medical_records = findDog(name).getMedicalRecords();
+        medical_records.add(0, records);
+        mongoTemplate.updateFirst(Query.query(Criteria.where("name").is(name)),
+                Update.update("medicalRecords", medical_records),
+                Dog.class);
+    }
+}
